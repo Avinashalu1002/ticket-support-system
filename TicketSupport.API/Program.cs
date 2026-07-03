3775c9b497e31142da34a512d8a11d9d39f5a75c
Avinash Damu <avinashabbi777@gmail.com>
2026-07-03 12:22:12 +0530

    Switch to PostgreSQL, add port binding, fix secrets

--- TicketSupport.API/Program.cs
+++ TicketSupport.API/Program.cs
@@ -85,7 +85,7 @@ builder.Services.AddSwaggerGen(options =>
 });
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(
+    options.UseNpgsql(
         builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IUserRepository, UserRepository>();
